062ca633893a465ed8251916fa6fba487ad6869a
yutthapichai chaiwong <yutthapichai.c@skanhubrewards.com>
2022-06-08 15:49:17 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -15,6 +15,7 @@ export default function Home() {
     <Default>
       <div>
         <button
+          className="btn btn-outline-info"
           onClick={() => {
             getBoards();
           }}
