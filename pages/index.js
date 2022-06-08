e59d811b79aa2bc0292cd07851b0bf697a88ab3d
yutthapichai chaiwong <yutthapichai.c@skanhubrewards.com>
2022-06-08 15:48:34 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,27 @@
 import Head from "next/head";
 import Default from "../components/layouts/Default";
+import Axios from "axios";
 
 export default function Home() {
-  return <Default>test</Default>;
+  const getBoards = async () => {
+    await Axios.get("http://www.matchchemical.tk:4007/v1/getBoards").then(
+      (res) => {
+        console.log(res.data);
+      }
+    );
+  };
+
+  return (
+    <Default>
+      <div>
+        <button
+          onClick={() => {
+            getBoards();
+          }}
+        >
+          TEST GET DATA
+        </button>
+      </div>
+    </Default>
+  );
 }
